8aae282a0747b346ed2957c7e619705a33cab2ed
infabls <infabls.com@gmail.com>
2020-07-03 17:37:20 +0300

    start working

---
+++ footer.php
@@ -0,0 +1,78 @@
+     
+
+			<!-- Footer -->
+			<footer class="footer bg-dark-overlay" style="background-image: url(img/footer/1.jpg);">
+				<div class="container-fluid">
+					<div class="footer__widgets">
+						<div class="row">
+
+							<div class="col-lg-3 col-md-3">
+								<div class="widget widget-about-us">
+									<!-- Logo -->
+									<a href="index.html" class="logo-container flex-child">
+										<img class="logo" src="img/logo_white.png" srcset="img/logo_white.png 1x, img/logo_white@2x.png 2x" alt="logo">
+									</a>
+								</div>
+							</div> <!-- end logo -->
+
+							<div class="col-lg-2 col-md-3">
+								<div class="widget widget_nav_menu">
+									<ul>
+										<li><a href="#">About</a></li>
+										<li><a href="#">Services</a></li>
+										<li><a href="#">Blog</a></li>
+										<li><a href="#">Contact</a></li>
+									</ul>
+								</div>
+							</div>
+
+							<div class="col-lg-2 col-md-3">
+								<div class="widget widget_nav_menu">
+									<ul>
+										<li><a href="#">Privacy Policy</a></li>
+										<li><a href="#">Terms &amp; Conditions</a></li>
+										<li><a href="#">Career</a></li>
+									</ul>
+								</div>
+							</div>
+
+							<div class="col-lg-3 offset-lg-2 col-md-3">
+								<div class="widget">
+									<div class="socials">
+										<a href="#" class="social social-twitter" aria-label="twitter" title="twitter" target="_blank"><i class="ui-twitter"></i></a>
+										<a href="#" class="social social-facebook" aria-label="facebook" title="facebook" target="_blank"><i class="ui-facebook"></i></a>
+										<a href="#" class="social social-youtube" aria-label="youtube" title="google plus" target="_blank"><i class="ui-youtube"></i></a>
+										<a href="#" class="social social-instagram" aria-label="instagram" title="instagram" target="_blank"><i class="ui-instagram"></i></a>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div> <!-- end container -->
+
+				<div class="footer__bottom">
+					<div class="container-fluid text-right text-md-center">
+						<span class="copyright">
+							&copy; 2018 Sedona, Made by <a href="https://deothemes.com">DeoThemes</a>
+						</span>
+					</div>
+				</div> <!-- end footer bottom -->
+			</footer> <!-- end footer -->
+
+			<div id="back-to-top">
+				<a href="#top"><i class="ui-arrow-up"></i></a>
+			</div>
+
+		</div> <!-- end content wrapper -->
+	</main> <!-- end main wrapper -->
+
+
+	<!-- jQuery Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/scripts.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Sedona | Elements</title>
+
+	<meta charset="utf-8">
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Barlow:400,600%7COpen+Sans:400,400i,700' rel='stylesheet'>
+
+	<!-- Css -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/font-icons.css" />
+	<link rel="stylesheet" href="revolution/css/settings.css" />
+	<link rel="stylesheet" href="css/style.css" />
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+</head>
+
+<body>
+
+	<!-- Preloader -->
+	<div class="loader-mask">
+		<div class="loader">
+			"Loading..."
+		</div>
+	</div>
+
+	<main class="main-wrapper">
+
+		<!-- Navigation -->
+		<header class="nav">
+			<div class="nav__holder nav--sticky">
+				<div class="container-fluid nav__container nav__container--side-padding">
+					<div class="flex-parent">
+
+						<div class="nav__header">
+							<!-- Logo -->
+							<a href="index.html" class="logo-container flex-child">
+								<img class="logo" src="img/logo.png" srcset="img/logo.png 1x, img/logo@2x.png 2x" alt="logo">
+							</a>
+
+							<!-- Mobile toggle -->
+							<button type="button" class="nav__icon-toggle" id="nav__icon-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="nav__icon-toggle-bar"></span>
+								<span class="nav__icon-toggle-bar"></span>
+								<span class="nav__icon-toggle-bar"></span>
+							</button>
+						</div>
+
+						<!-- Navbar -->
+						<nav id="navbar-collapse" class="nav__wrap collapse navbar-collapse">
+							<ul class="nav__menu">
+								<li class="nav__dropdown active">
+									<a href="index.html" aria-haspopup="true">Home</a>
+									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
+									<ul class="nav__dropdown-menu">
+										<li><a href="index.html">Home 1</a></li>
+										<li><a href="index-2.html">Home 2</a></li>
+										<li><a href="index-3.html">Home 3</a></li>
+										<li><a href="index-4.html">Home 4</a></li>
+									</ul>
+								</li>
+								<li class="nav__dropdown">
+									<a href="#" aria-haspopup="true">Pages</a>
+									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
+									<ul class="nav__dropdown-menu">
+										<li><a href="about.html">About</a></li>
+										<li><a href="services.html">Services</a></li>
+										<li><a href="single-service.html">Single Service</a></li>
+										<li><a href="contact.html">Contact</a></li>
+									</ul>
+								</li>
+								<li class="nav__dropdown">
+									<a href="#" aria-haspopup="true">Works</a>
+									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
+									<ul class="nav__dropdown-menu">
+										<li><a href="portfolio.html">Portfolio</a></li>
+										<li><a href="portfolio-gallery.html">Portfolio Gallery</a></li>
+										<li><a href="portfolio-single.html">Portfolio Single</a></li>
+									</ul>
+								</li>
+								<li class="nav__dropdown">
+									<a href="#" aria-haspopup="true">Features</a>
+									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
+									<ul class="nav__dropdown-menu">
+										<li><a href="icons.html">Icons</a></li>
+										<li><a href="elements.html">Elements</a></li>
+									</ul>
+								</li>
+								<li class="nav__dropdown">
+									<a href="blog.html" aria-haspopup="true">Blog</a>
+									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
+									<ul class="nav__dropdown-menu">
+										<li><a href="blog-masonry.html">Blog Masonry</a></li>
+										<li><a href="blog.html">Blog Standard</a></li>
+										<li><a href="single-post.html">Single Post</a></li>
+									</ul>
+								</li>
+							</ul> <!-- end menu -->
+							<div class="nav__phone nav__phone--mobile d-lg-none">
+								<span class="nav__phone-text">Call us:</span>
+								<a href="tel:1-800-995-3959" class="nav__phone-number">1-800-995-3959</a>
+							</div>
+
+							<div class="nav__socials nav__socials--mobile d-lg-none">
+								<div class="socials">
+									<a href="#" class="social social-twitter" aria-label="twitter" title="twitter" target="_blank"><i class="ui-twitter"></i></a>
+									<a href="#" class="social social-facebook" aria-label="facebook" title="facebook" target="_blank"><i class="ui-facebook"></i></a>
+									<a href="#" class="social social-youtube" aria-label="youtube" title="google plus" target="_blank"><i class="ui-youtube"></i></a>
+									<a href="#" class="social social-instagram" aria-label="instagram" title="instagram" target="_blank"><i class="ui-instagram"></i></a>
+								</div>
+							</div>
+						</nav> <!-- end nav-wrap -->
+
+						<div class="nav__phone nav--align-right d-none d-lg-block">
+							<span class="nav__phone-text">Call us:</span>
+							<a href="tel:1-800-995-3959" class="nav__phone-number">1-800-995-3959</a>
+						</div>
+
+						<div class="nav__socials d-none d-lg-block">
+							<div class="socials">
+								<a href="#" class="social social-twitter" aria-label="twitter" title="twitter" target="_blank"><i class="ui-twitter"></i></a>
+								<a href="#" class="social social-facebook" aria-label="facebook" title="facebook" target="_blank"><i class="ui-facebook"></i></a>
+								<a href="#" class="social social-youtube" aria-label="youtube" title="google plus" target="_blank"><i class="ui-youtube"></i></a>
+								<a href="#" class="social social-instagram" aria-label="instagram" title="instagram" target="_blank"><i class="ui-instagram"></i></a>
+							</div>
+						</div>
+
+					</div> <!-- end flex-parent -->
+				</div> <!-- end container -->
+
+			</div>
+		</header> <!-- end navigation -->
+
