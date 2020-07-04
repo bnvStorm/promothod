65eb6b2d0202f4779a3dcd80717441a6157adf81
infabls <infabls.com@gmail.com>
2020-07-04 18:01:14 +0300

    add photos, add switch sidebar func

--- template.php
+++ template.php
@@ -3,6 +3,32 @@
 
 		<div class="content-wrapper content-wrapper--boxed oh">
 
+		<?php 
+			$cat = explode('/', $_SERVER['REQUEST_URI']);
+			$cat = $cat[1];
+		switch ($cat) {
+			case 'otkhody':
+				# code...
+				break;
+			case 'o-nas':
+				# code...
+				break;
+			case 'uslugi':
+				# code...
+				break;
+			case 'info':
+				# code...
+				break;
+			case 'kontakty':
+				# code...
+				break;
+			
+			default:
+				# code...
+				break;
+		}  ?>
+		<h1><?php echo $h1; ?></h1>
+		
 		<?php echo $content; ?>
 		
 
