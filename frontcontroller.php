468e32f6f4a558d2ef28af02fe43794a59977252
infabls <infabls.com@gmail.com>
2020-07-05 15:45:52 +0300

    add comments to frontcontroller

--- frontcontroller.php
+++ frontcontroller.php
@@ -2,6 +2,7 @@
 // config data
 require 'config.php';
 
+// выборка всего из бд по найденному урлу
 $sql = "SELECT * FROM promothod_kz___promothod_kz WHERE url = '$url'";
 
 // MySQLi Object-oriented
@@ -12,13 +13,17 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+// выполнение запроса
 $result = $conn->query($sql);
+// если результатов не найдено
 if ($result->num_rows < 1) {
-	header('Location: http://www.promothod.kz/404.html');
+// редирект на 404 ошибку
+	header('Location: '. $base_uri . '404.php');
   	exit;
 }
+// если результат есть
 if ($result->num_rows > 0) {
-  // output data of each row
+  // присваиваем значения переменных и закрываем соединение
   while($row = $result->fetch_assoc()) {
     $title = $row['title'];
     $descr = $row['descr'];
@@ -29,16 +34,5 @@ if ($result->num_rows > 0) {
 }  
 $conn->close();
 
-
-
-
+// подтягиваем шаблон
 require 'template.php';
-
-
-
-
-
-
-// else{
-// 	
-// }
