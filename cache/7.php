23d4132dce161a376c2c0bbfb36b6ddf6369f69c
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-07-07 19:59:34 +0600

    upd bd

---
+++ cache/7.php
@@ -0,0 +1,60 @@
+
+<div class="about">
+  <p align="justify" class="MsoNormal"><font size="2" face="Arial">ТОО «ПРОМОТХОД КАЗАХСТАН» - это специализированная компания по обращению с отходами производства и потребления.</font></p>
+ 
+<p> </p>
+ 
+<p align="justify" class="MsoNormal"><font size="2" face="Arial">Наша компания предлагает полный&nbsp;</font><a href="/uslugi/" title="ПЕРЕЙТИ К УСЛУГАМ">перечень услуг</a><font size="2" face="Arial">&nbsp;по обращению с отходами:</font></p>
+ 
+<p> </p>
+  <img class="about_img" src="../img/vyvoz_ewaste.JPG" alt="Kazpromothod">
+<ul> 
+  <li><font size="2" face="Arial">Документирование отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Упаковка отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Временное хранение отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Продажа и/или предоставление контейнеров для сбора;</font></li>
+ 
+  <li><font size="2" face="Arial">Сбор отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Транспортировка отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Переработка отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Реализация продуктов переработки;</font></li>
+ 
+  <li><font size="2" face="Arial">Уничтожение отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Захоронение отходов;</font></li>
+ 
+  <li><font size="2" face="Arial">Устранение загрязнений, возникших в результате неправильного обращения с отходами;</font></li>
+ 
+  <li><font size="2" face="Arial">Продажа оборудования и материалов для обращения с отходами. </font></li>
+ </ul>
+ 
+<p> </p>
+
+<p align="justify" class="MsoNormal"><font size="2" face="Arial">Основная промышленная площадка ТОО «ПРОМОТХОД КАЗАХСТАН» - «Завод по утилизации опасных промышленных отходов», расположен в г. Караганда. Наша компания оснащена современным оборудованием для переработки большого перечня&nbsp;</font><a href="/otkhody/">отходов</a><font size="2" face="Arial">&nbsp;производства и потребления. Все используемое оборудование соответствует нормативным требованиям Республики Казахстан.</font></p>
+ 
+<p> </p>
+ 
+<p align="justify" class="MsoNormal"><font size="2" face="Arial">Мы имеем развитую сеть филиалов и&nbsp;</font><a href="/o-nas/predstavitelstva/">представительств</a><font size="2" face="Arial">&nbsp;в крупнейших городах Республики Казахстан.</font></p>
+ 
+<p align="justify"> </p>
+ 
+<p align="justify" class="MsoNormal"><font size="2" face="Arial">Для транспортировки опасных промышленных отходов используется специализированный транспорт.</font></p>
+ 
+<p> </p>
+ 
+<p align="justify" class="MsoNormal"><font size="2" face="Arial">ТОО «ПРОМОТХОД КАЗАХСТАН» имеет все необходимые <a href="http://www.promothod.kz/o-nas/razreshitelnye-dokumenty/"><font color="#0000FF"><u><b>разрешительные документы</b></u></font></a> и лицензии для осуществления деятельности по обращению с отходами производства и потребления.</font></p>
+ 
+<p align="justify"> </p>
+ 
+<p align="justify" class="MsoNormal"><font size="2" face="Arial">В нашей компании работает квалифицированный персонал, прошедший специальное обучение по обращению с отходами производства и потребления.</font></p>
+ 
+<p align="justify"> </p>
+ 
+<p align="justify" class="MsoNormal"><font size="2" face="Arial">ТОО «ПРОМОТХОД КАЗАХСТАН» постоянно расширяет перечень оказываемых услуг и используемых технологий для более безопасного обращения с отходами производства и потребления.<span>&nbsp; </span></font></p>
+</div>
