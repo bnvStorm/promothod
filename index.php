513b3e64eeca84ef0152484b8e8fa423813ca75c
infabls <infabls.com@gmail.com>
2020-07-08 18:57:38 +0300

    remove rev slider from all page excluded index.php

--- index.php
+++ index.php
@@ -591,6 +591,101 @@
 					</div>
 				</div>
 			</section> <!-- end from blog -->
-
 			<!-- Footer -->
-<?php include 'footer.php' ?>
+			<footer class="footer bg-dark-overlay">
+				<div class="container-fluid">
+					<div class="footer__widgets">
+						<div class="row">
+
+							<div class="col-lg-3 col-md-3">
+								<div class="widget widget-about-us">
+									<!-- Logo -->
+									<a href="/" class="logo-container flex-child">
+										<img class="logo" src="/img/logo.png" srcset="/img/logo.png 1x, img/logo_white@2x.png 2x" alt="logo">
+									</a>
+								</div>
+							</div> <!-- end logo -->
+
+							<div class="col-lg-9 col-md-9 fmenu">
+								<div class="widget widget_nav_menu">
+									<ul class="footer-list">
+
+											<li><a href="/o-nas/">О нас</a></li>								
+
+											<li><a href="/o-nas/otzyvy/">Отзывы</a></li>						
+
+											<li><a href="/uslugi/">Услуги</a></li>									
+
+											<li><a href="/otkhody/">Отходы</a></li>									
+
+											<li><a href="/info/">Инфо (обращение с отходами)</a></li>				
+											
+											<li><a href="/o-nas/novosti/">Новости</a></li>							
+
+											<li><a href="/tseny/">Цены</a></li>										
+
+											<li><a href="/resheniya/">Решения</a></li>								
+
+											<li><a href="/sitemap/">Карта сайта</a></li>
+						
+
+									</ul>
+								</div>
+							</div>
+							</div>
+<!-- 
+							<div class="col-lg-3 offset-lg-2 col-md-3">
+								<div class="widget">
+									<div class="socials">
+										<a href="#" class="social social-twitter" aria-label="twitter" title="twitter" target="_blank"><i class="ui-twitter"></i></a>
+										<a href="#" class="social social-facebook" aria-label="facebook" title="facebook" target="_blank"><i class="ui-facebook"></i></a>
+										<a href="#" class="social social-youtube" aria-label="youtube" title="google plus" target="_blank"><i class="ui-youtube"></i></a>
+										<a href="#" class="social social-instagram" aria-label="instagram" title="instagram" target="_blank"><i class="ui-instagram"></i></a>
+									</div>
+								</div>
+							</div> -->
+
+						</div>
+					</div>
+				</div> <!-- end container -->
+
+				<div class="footer__bottom">
+					<div class="container-fluid text-right text-md-center">
+						<span class="copyright">
+							&copy; 2020 Караганда, Казпромотход</a>
+						</span>
+					</div>
+				</div> <!-- end footer bottom -->
+			</footer> <!-- end footer -->
+
+			<div id="back-to-top">
+				<a href="#top"><i class="ui-arrow-up"></i></a>
+			</div>
+
+		</div> <!-- end content wrapper -->
+	</main> <!-- end main wrapper -->
+
+
+	<!-- jQuery Scripts -->
+	<script src="/js/jquery.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+	<script src="/js/plugins.js"></script>
+	<script src="/revolution/js/jquery.themepunch.tools.min.js"></script>
+	<script src="/revolution/js/jquery.themepunch.revolution.min.js"></script>
+	<script src="/js/rev-slider.js"></script>
+	<script src="/js/scripts.js"></script>
+
+
+	<!-- Rev Slider Offline Scripts -->
+	<script src="/revolution/js/extensions/revolution.extension.video.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+
+</body>
+</html>
