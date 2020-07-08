ff08411255fb262420262ef5569137ea403e7a38
infabls <infabls.com@gmail.com>
2020-07-09 02:35:15 +0300

    compress images again, remove revslider plugins

--- index.php
+++ index.php
@@ -677,15 +677,14 @@
 
 
 	<!-- Rev Slider Offline Scripts -->
-	<script src="/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<!-- 	<script src="/revolution/js/extensions/revolution.extension.video.min.js"></script>
 	<script src="/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
 	<script src="/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
 	<script src="/revolution/js/extensions/revolution.extension.actions.min.js"></script>
 	<script src="/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
 	<script src="/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
 	<script src="/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-	<script src="/revolution/js/extensions/revolution.extension.migration.min.js"></script>
-	<script src="/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+	<script src="/revolution/js/extensions/revolution.extension.parallax.min.js"></script> -->
 
 </body>
 </html>
