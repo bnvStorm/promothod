af073cf003deff9efa2ea8b7061e279c2ac97e1f
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-07-09 21:19:26 +0600

    upd js

--- js/site.addons.js
+++ js/site.addons.js
@@ -5,5 +5,24 @@
         {
             $(this).addClass('active');
         }
+      if($(".leftMenu a").hasClass("active")){
+		$('.nav__dropdown-menu').addClass('disactive');
+	}
     });
 });
+// // путь в урле
+//   var pathname  = window.location.pathname;
+//   $(document).ready(function(){
+//    // все ссылки из бокового меню
+//    var links = $('.leftMenu a');
+//    // links.parents().siblings('i').click()
+//    // цикл обработки ссылок
+//    for (var i = links.length - 1; i >= 0; i--) {
+//     // проверка на совпадение урла и ссылки
+//     if (links[i].pathname === pathname) {
+//      // кликнуть по иконке в ближайшем родителе
+//      $(links[i]).parents().siblings('i').click()
+//     }
+    
+//    }
+//   });
