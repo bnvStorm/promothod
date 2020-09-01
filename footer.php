03ae83e77fa015c69c1a0ecf9d6025ecda523f7c
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-09-01 15:13:30 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,106 +13,18 @@
 								</div>
 							</div> <!-- end logo -->
 
-							<div class="col-lg-9 col-md-9 fmenu">
+								<div class="col-lg-9 col-md-9 fmenu">
 								<div class="widget widget_nav_menu">
-								<!-- 	<ul class="footer-list">
-
-											<li><a href="/o-nas/">О нас</a></li>								
-
-											<li><a href="/o-nas/otzyvy/">Отзывы</a></li>						
-
-											<li><a href="/uslugi/">Услуги</a></li>									
-
-											<li><a href="/otkhody/">Отходы</a></li>									
-
-											<li><a href="/info/">Инфо (обращение с отходами)</a></li>				
-											
-											<li><a href="/o-nas/novosti/">Новости</a></li>							
-
-											<li><a href="/tseny/">Цены</a></li>										
-
-											<li><a href="/resheniya/">Решения</a></li>								
-
-											<li><a href="/sitemap/">Карта сайта</a></li>
-						
-
-										</ul> -->
-										<nav id="navbar-collapse" class="nav__wrap collapse navbar-collapse nav_top">
-											<ul class="nav__menu">
-												<li class="nav__dropdown">
-													<a href="/" aria-haspopup="true">Главная</a>
-													<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
-									<!-- <ul class="nav__dropdown-menu">
-										<li><a href="index.html">О нас</a></li>
-										<li><a href="index-2.html">Home 2</a></li>
-										<li><a href="index-3.html">Home 3</a></li>
-										<li><a href="index-4.html">Home 4</a></li>
-									</ul> -->
-								</li>
-								<li class="nav__dropdown">
-									<a href="/o-nas/" aria-haspopup="true">О нас</a>
-									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
-									<!-- <ul class="nav__dropdown-menu">
-										<li><a href="index.html">О нас</a></li>
-										<li><a href="index-2.html">Home 2</a></li>
-										<li><a href="index-3.html">Home 3</a></li>
-										<li><a href="index-4.html">Home 4</a></li>
-									</ul> -->
-								</li>
-								<li class="nav__dropdown">
-									<a href="/uslugi/" aria-haspopup="true">Услуги</a>
-									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
-						<!-- 			<ul class="nav__dropdown-menu">
-										<li><a href="about.html">About</a></li>
-										<li><a href="services.html">Services</a></li>
-										<li><a href="single-service.html">Single Service</a></li>
-										<li><a href="contact.html">Contact</a></li>
-									</ul> -->
-								</li>
-								<li class="nav__dropdown">
-									<a href="/otkhody/" aria-haspopup="true">Отходы</a>
-									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
-								<!-- 	<ul class="nav__dropdown-menu">
-										<li><a href="portfolio.html">Portfolio</a></li>
-										<li><a href="portfolio-gallery.html">Portfolio Gallery</a></li>
-										<li><a href="portfolio-single.html">Portfolio Single</a></li>
-									</ul> -->
-								</li>
-								<li class="nav__dropdown">
-									<a href="/zakon/" aria-haspopup="true">Закон</a>
-									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
-								<!-- 	<ul class="nav__dropdown-menu">
-										<li><a href="icons.html">Icons</a></li>
-										<li><a href="elements.html">Elements</a></li>
-									</ul> -->
-								</li>
-								<li class="nav__dropdown">
-									<a href="/kontakty/" aria-haspopup="true">Контакты</a>
-									<i class="ui-arrow-down nav__dropdown-trigger" role="button" aria-haspopup="true" aria-expanded="false"></i>
-								<!-- 	<ul class="nav__dropdown-menu">
-										<li><a href="blog-masonry.html">Blog Masonry</a></li>
-										<li><a href="blog.html">Blog Standard</a></li>
-										<li><a href="single-post.html">Single Post</a></li>
-									</ul> -->
-								</li>
-							</ul> <!-- end menu -->
-							<div class="nav__phone nav__phone--mobile d-lg-none">
-								<span class="nav__phone-text">Позвоните нам:</span>
-								<a href="tel: 8 (7212) 41-00-29" class="nav__phone-number"> 8 (7212) 41-00-29</a>
-							</div>
-
-							<div class="nav__socials nav__socials--mobile d-lg-none">
-								<div class="socials">
-									<!-- <a href="#" class="social social-twitter" aria-label="twitter" title="twitter" target="_blank"><i class="ui-twitter"></i></a> -->
-									<a href="#" class="social social-facebook" aria-label="facebook" title="facebook" target="_blank"><i class="ui-facebook"></i></a>
-									<!-- <a href="#" class="social social-youtube" aria-label="youtube" title="google plus" target="_blank"><i class="ui-youtube"></i></a> -->
-									<a href="#" class="social social-instagram" aria-label="instagram" title="instagram" target="_blank"><i class="ui-instagram"></i></a>
+									<ul class="footer-list">
+										<li><a href="/" aria-haspopup="true">Главная</a></li>
+										<li><a href="/o-nas/" aria-haspopup="true">О нас</a></li>
+										<li><a href="/uslugi/" aria-haspopup="true">Услуги</a></li>
+										<li><a href="/otkhody/" aria-haspopup="true">Отходы</a></li>
+										<li><a href="/zakon/" aria-haspopup="true">Закон</a></li>
+										<li><a href="/kontakty/" aria-haspopup="true">Контакты</a></li>
+									</ul>
 								</div>
 							</div>
-
-						</nav> <!-- end nav-wrap -->
-					</div>
-				</div>
 			</div>
 <!-- 
 							<div class="col-lg-3 offset-lg-2 col-md-3">
