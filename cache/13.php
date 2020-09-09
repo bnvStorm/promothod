8ae416782deff1600a40d88356ccd0cb9c96bf01
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-09-09 14:26:32 +0600

    Update 13.php

--- cache/13.php
+++ cache/13.php
@@ -1,12 +1,12 @@
 <div class="video-item">
-    <iframe max-width="680" width="100%" height="315" src="http://www.youtube.com/embed/JSi5X7Vs3Y0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe max-width="680" width="100%" height="315" src="https://www.youtube.com/embed/JSi5X7Vs3Y0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 <!-- <div class="video-item" >
     <iframe width="680" height="315" src="http://www.youtube.com/embed/WpZQoaZII1M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div> -->
 
 <div class="video-item">
-    <iframe max-width="680" width="100%" height="315" src="http://www.youtube.com/embed/givNO35dRBQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe max-width="680" width="100%" height="315" src="https://www.youtube.com/embed/givNO35dRBQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
 <div class="video-item">
