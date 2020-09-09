62e25539d8928ca0a4bcff2fb553d4201cf6e756
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-09-09 23:50:38 +0600

    Update 86.php

--- cache/86.php
+++ cache/86.php
@@ -20,10 +20,10 @@
 	</div>
 	<div class="people_item">
 		<div class="people_img">
-			<img src="../../img/new/sovet/%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%20%D0%A2%D1%8E%D1%85%D1%82%D0%B8%D0%BD.jpg" alt="">
+			<img style="transform: scale(-1, 1);" src="../../img/new/sovet/%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%20%D0%A2%D1%8E%D1%85%D1%82%D0%B8%D0%BD.jpg" alt="">
 		</div>
 		<div class="people_text">
-			<div class="people_text-bold">Вячеслав ТЮХТИН
+			<div class="people_text-bold" >Вячеслав ТЮХТИН
 
 			</div>
 			<div class="people_text-regular">директор ТОО «Казахстанский оператор по управлению отходами»</div>
@@ -86,10 +86,10 @@
 	</div>	
 	<div class="people_item">
 		<div class="people_img">
-			<img src="../../img/new/sovet/%D0%95%D0%B2%D0%B3%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%A2%D1%83%D1%80.jpg" alt="">
+			<img style="transform: scale(-1, 1);" src="../../img/new/sovet/%D0%95%D0%B2%D0%B3%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%A2%D1%83%D1%80.jpg" alt="">
 		</div>
 		<div class="people_text">
-			<div class="people_text-bold">Евгения ТУР
+			<div class="people_text-bold" >Евгения ТУР
 
 			</div>
 			<div class="people_text-regular">директор «Промышленные отходы»</div>
