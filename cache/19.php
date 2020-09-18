91501c2edb2171624ac932d476db6eabc9144811
infabls <infabls.com@gmail.com>
2020-09-18 16:19:01 +0600

    lazyload other page`s photos

--- cache/19.php
+++ cache/19.php
@@ -28,61 +28,61 @@
 
 <div class="block_doc">
   <div class="item">
-    <a data-fancybox="gallery" href="../../files/doc/1.png"> <img src="../../files/doc/1.png" alt="">  </a>
+    <a data-fancybox="gallery" href="../../files/doc/1.png"> <img class="lazyload" data-src="/files/doc/1.png" alt="">  </a>
 <div class="text">
    ЛИЦЕНЗИЯ НА ЭКСПОРТ ОТДЕЛЬНЫХ ВИДОВ ТОВАРОВ
  </div>
  </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/2.png"> <img src="../../files/doc/2.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/2.png"> <img class="lazyload" data-src="/files/doc/2.png" alt="">   </a>
  <div class="text">
   Министерство экологии РК-заключение перевозка ПХД
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/3.png"> <img src="../../files/doc/3.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/3.png"> <img class="lazyload" data-src="/files/doc/3.png" alt="">   </a>
  <div class="text">
   ЛИЦЕНЗИЯ НА ЭКСПОРТ ОТДЕЛЬНЫХ ВИДОВ ТОВАРОВ <br>КОНДЕНС_ФРАГМ-01-05-2020
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/4.png"> <img src="../../files/doc/4.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/4.png"> <img class="lazyload" data-src="/files/doc/4.png" alt="">   </a>
  <div class="text">
   ЛИЦЕНЗИЯ НА ЭКСПОРТ ОТДЕЛЬНЫХ ВИДОВ ТОВАРОВ <br>Лицен_ТРАНСФ-01-05-2020
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/5.png"> <img src="../../files/doc/5.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/5.png"> <img class="lazyload" data-src="/files/doc/5.png" alt="">   </a>
  <div class="text">
   ЛИЦЕНЗИЯ НА ЭКСПОРТ ОТДЕЛЬНЫХ ВИДОВ ТОВАРОВ <br>Лицен_ВЫКЛЮЧ_01-05-2020
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/6.png"> <img src="../../files/doc/6.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/6.png"> <img class="lazyload" data-src="/files/doc/6.png" alt="">   </a>
  <div class="text">
   ЛИЦЕНЗИЯ НА ЭКСПОРТ ОТДЕЛЬНЫХ ВИДОВ ТОВАРОВ <br>Лицен_СИЗ-01-05-2020
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/7.png"> <img src="../../files/doc/7.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/7.png"> <img class="lazyload" data-src="/files/doc/7.png" alt="">   </a>
  <div class="text">
   ЛИЦЕНЗИЯ НА ЭКСПОРТ ОТДЕЛЬНЫХ ВИДОВ ТОВАРОВ <br>Лицен_МАСЛА-01-05-2020
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/8.png"> <img src="../../files/doc/8.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/8.png"> <img class="lazyload" data-src="/files/doc/8.png" alt="">   </a>
  <div class="text">
   Уведомление о трансгран.перевозке ПХД-Бельгия
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/9.png"> <img src="../../files/doc/9.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/9.png"> <img class="lazyload" data-src="/files/doc/9.png" alt="">   </a>
  <div class="text">
   Свидетельство о перерегистрации 21.05.2013г.
 </div>
 </div>
 <div class="item">
-  <a data-fancybox="gallery" href="../../files/doc/10.png"> <img src="../../files/doc/10.png" alt="">   </a>
+  <a data-fancybox="gallery" href="../../files/doc/10.png"> <img class="lazyload" data-src="/files/doc/10.png" alt="">   </a>
  <div class="text">
   Справка о гос. перерегистрации от 25.07.19г.
 </div>
